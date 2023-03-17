009edab6d3c699032d5ce1bed9fe63fb1323c75b
Lawrence Neal <nealla@lwneal.com>
2023-03-17 09:31:51 -0700

    Rename to gptswe
    It's like an automatic GPT software engineer

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="gptrepo",
-    version="1.3.0",
+    name="gptswe",
+    version="1.0.0",
     packages=find_packages(),
     install_requires=[
         'pathspec',
@@ -10,7 +10,7 @@ setup(
         ],
     entry_points={
         "console_scripts": [
-            "gptrepo=gptrepo.gptrepo:main",
+            "gptswe=gptswe.gptswe:main",
         ]
     },
 )
