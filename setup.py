f08cfc3f1ad2422250b3a0ad491da00f990e9548
Lawrence Neal <nealla@lwneal.com>
2023-03-16 19:47:42 -0700

    Shorten name (to save tokens), make it a command
    python setup.py install && gptrepo

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="gpt-repository-loader",
-    version="0.2.0",
+    name="gptrepo",
+    version="1.0.0",
     packages=find_packages(),
     install_requires=[],
     entry_points={
         "console_scripts": [
-            "gpt_repository_loader = gpt_repository_loader:main",
+            "gptrepo = gptrepo.gptrepo:main",
         ]
     },
 )
