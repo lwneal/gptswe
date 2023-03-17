a76cdb3d1a2cb67c756fc732455ed11a6fbc4776
Lawrence Neal <nealla@lwneal.com>
2023-03-16 22:44:16 -0700

    Simplify ignore parsing code

--- gptrepo/gptrepo.py
+++ gptrepo/gptrepo.py
@@ -7,21 +7,24 @@ from pathspec.patterns import GitWildMatchPattern
 
 PRELUDE="The following text is a Git repository with code. The structure of the text are sections that begin with ----, followed by a single line containing the file path and file name, followed by a variable amount of lines containing the file contents. The text representing the Git repository ends when the symbols --END-- are encounted. Any further text beyond --END-- are meant to be interpreted as instructions using the aforementioned Git repository as context.\n"
 
-def get_ignore_list(ignore_paths):
-    return [line.strip() for path in ignore_paths if os.path.exists(path) for line in open(path, 'r')]
 
-def should_ignore(file_path, ignore_spec):
-    return ignore_spec.match_file(file_path)
+def get_ignore_spec(repo_path, ignore_paths):
+    dotignores = [os.path.join(repo_path, i) for i in ignore_paths]
+    ignore_list = [line.strip() for path in dotignores if os.path.exists(path)
+                   for line in open(path, 'r')]
+    return PathSpec.from_lines(GitWildMatchPattern, ignore_list)
+
 
 def process_repository(repo_path, ignore_spec, fp):
     for root, _, files in os.walk(repo_path):
         for file in files:
             file_path = os.path.join(root, file)
             relpath = os.path.relpath(file_path, repo_path)
-            if not should_ignore(relpath, ignore_spec):
+            if not ignore_spec.match_file(relpath):
                 with open(file_path, 'r', errors='ignore') as file:
                     contents = file.read()
                 fp.write(f"----\n{relpath}\n{contents}\n")
+
 
 def main():
     par = argparse.ArgumentParser(description="Convert a Git repository into a text format")
@@ -29,8 +32,9 @@ def main():
     par.add_argument("-o", "--output", default=None, help="Path to the output file")
     par.add_argument("--ignore", nargs='*', default=[".gptignore", ".gitignore"], help="Paths to the ignore files")
     args = par.parse_args()
-    ignore_list = get_ignore_list([os.path.join(args.repo_path, i) for i in args.ignore])
-    ignore_spec = PathSpec.from_lines(GitWildMatchPattern, ignore_list)
+
+
+    ignore_spec = get_ignore_spec(args.repo_path, args.ignore)
 
     with (open(args.output, 'w') if args.output else sys.stdout) as fp:
         process_repository(args.repo_path, ignore_spec, fp)
