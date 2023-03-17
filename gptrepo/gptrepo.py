cdfb418e78889c99d31b7742ac704fafc0f0ed76
Lawrence Neal <nealla@lwneal.com>
2023-03-16 23:45:01 -0700

    Add token counting

--- gptrepo/gptrepo.py
+++ gptrepo/gptrepo.py
@@ -3,9 +3,10 @@ import sys
 import argparse
 from pathspec import PathSpec
 from pathspec.patterns import GitWildMatchPattern
+import gptwc
 
 PREPROMPT = "Please read all of the following files. Each file begins with a filename followed by ```` and the sequence of files ends with ``END``\n"
-POSTPROMPT = "Instructions: Read the above code. Identify any obvious bugs or security issues and suggest fixes in a terse, elegant, professional style. Show me each fixed line of code, with comments explaining each fix.\n"
+INSTRUCTION = "Instructions: Read the above code. Identify any obvious bugs or security issues and suggest fixes in a terse, elegant, professional style. Show me each fixed line of code, with comments explaining each fix.\n"
 
 
 def get_ignore_spec(repo_path, ignore_paths):
@@ -17,6 +18,7 @@ def get_ignore_spec(repo_path, ignore_paths):
 
 def process_repository(repo_path, ignore, fp):
     ignore_spec = get_ignore_spec(repo_path, ignore)
+    token_count = 0
     for root, _, files in os.walk(repo_path):
         for file in files:
             file_path = os.path.join(root, file)
@@ -24,8 +26,13 @@ def process_repository(repo_path, ignore, fp):
             if not ignore_spec.match_file(relpath):
                 with open(file_path, 'r', errors="replace") as file:
                     contents = file.read()
-                fp.write(f"`````\n{relpath}\n````\n{contents}\n")
-    fp.write("````\n\n")
+                text = f"`````\n{relpath}\n````\n{contents}\n"
+                token_count += gptwc.token_count(text)
+                fp.write(text)
+    text = "````\n\n"
+    token_count += gptwc.token_count(text)
+    fp.write(text)
+    return token_count
 
 
 def main():
@@ -34,7 +41,7 @@ def main():
     par.add_argument("-o", "--output", default=None, help="Path to the output file")
     par.add_argument("--ignore", nargs='*', default=[".gptignore", ".gitignore"], help="Paths to the ignore files")
     par.add_argument("--preprompt", default=PREPROMPT, help="Text to be displayed before the repository")
-    par.add_argument("--postprompt", default=POSTPROMPT, help="Text to be displayed after the repository")
+    par.add_argument("-i", "--instruction", default=INSTRUCTION, help="Instruction text displayed at the end")
     args = par.parse_args()
 
     if args.output is None:
@@ -43,11 +50,11 @@ def main():
         fp = open(args.output, 'w')
 
     fp.write(args.preprompt)
-    process_repository(args.repo_path, args.ignore, fp)
-    fp.write(args.postprompt)
+    content_tokens = process_repository(args.repo_path, args.ignore, fp)
+    fp.write(args.instruction)
 
-    if args.output:
-        print(f"Repository contents written to {args.output}.")
+    token_count = gptwc.token_count(args.preprompt) + content_tokens + gptwc.token_count(args.instruction)
+    sys.stderr.write(f"\n{token_count} tokens written to {args.output or 'stdout'}\n")
 
 if __name__ == "__main__":
     main()
